f2e360ad7cf2d74253521cc71d5d8afad981e3af
emanoeldelfino <emanoeldelfno@gmail.com>
2021-03-10 23:54:37 -0300

    Add fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,20 @@
+function fizzBuzz(num) {
+    num = Number(num);
+
+    for (i=0; i <= num; i++) {
+        if (i % 3 === 0) {
+            document.write('Fizz');
+        }
+        if (i % 5 === 0) {
+            document.write('Buzz');
+        }
+
+        if (!(i % 3 === 0 && i % 5 === 0)) {
+            document.write(i);
+        }
+
+        document.write('<br>')
+    }
+}
+
+fizzBuzz(100);
